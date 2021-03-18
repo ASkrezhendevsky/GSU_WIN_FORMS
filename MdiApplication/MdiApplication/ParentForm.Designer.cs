b0052ec4f1cf0fc8c3e346c9fd81e9781495e6ad
Alexandr Skrezhendevsky <alekskrzh@gmail.com>
2021-03-18 17:40:25 +0300

    Form properties update

--- MdiApplication/MdiApplication/ParentForm.Designer.cs
+++ MdiApplication/MdiApplication/ParentForm.Designer.cs
@@ -34,9 +34,10 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(402, 273);
+            this.IsMdiContainer = true;
             this.Name = "ParentForm";
-            this.Text = "Form1";
+            this.Text = "ParentForm";
             this.ResumeLayout(false);
 
         }
